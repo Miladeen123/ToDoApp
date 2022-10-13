65811307bbb1ba814be6426e9b75aa68a9b351e0
Nikola Veselinović <nik-ves@hotmail.com>
2022-10-13 10:05:28 +0200

    NV - Pull request test

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import data from "./TestData.json";
 //components
 import Header from "./Header";
 import ToDoList from "./todolist";
-import './App.css';
- 
+import "./App.css";
+
+// test pull
+
 function App() {
-  const [ toDoList, setToDoList ] = useState(data);
- 
- 
- return (
-   <div    className="App">
-     <Header />
-     <ToDoList toDoList={toDoList}/>
-   </div>
- );
+  const [toDoList, setToDoList] = useState(data);
+
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList} />
+    </div>
+  );
 }
- 
+
 export default App;
